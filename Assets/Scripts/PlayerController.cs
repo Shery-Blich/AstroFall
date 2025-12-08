96fc9b230260571333c39182f0715fcfb33ce61d
Shery-Blich <62645308+Shery-Blich@users.noreply.github.com>
2025-12-08 19:40:38 +0200

    gfhfh

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 /// <summary>
 /// Hello world!!! :)
 /// </summary>
+/// // ueet
 public class PlayerController : MonoBehaviour
 {
     void Update()
