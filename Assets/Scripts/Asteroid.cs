6ad3eee4b04ba441a3cc595c4f7d8a931f5e9aa8
Shery-Blich <62645308+Shery-Blich@users.noreply.github.com>
2025-12-16 13:10:24 +0200

    Adding canvas prefab and making astroid size random, and supporting horiziontal

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    [SerializeField]
-    public float size;  
     public Rigidbody2D rb;
 
     public float minForce;
 
+    private float size;
+
     void Start()
     {
+        size = Random.Range(1.0f, 3.0f);
         transform.localScale = 0.3f * size * Vector3.one;
         minForce = 1.0f / size;
         AddForce(minForce, size);
