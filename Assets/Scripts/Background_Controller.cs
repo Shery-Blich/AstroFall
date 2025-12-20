3a45a930eac3c3233a438ed86837812ba8f085ce
Julia Ballo <>
2025-12-20 10:23:54 +0200

    Camera LS&Portrait controller, obstacle size

--- Assets/Scripts/Background_Controller.cs
+++ Assets/Scripts/Background_Controller.cs
@@ -6,7 +6,17 @@ public class Background_Controller : MonoBehaviour
     public Vector2 direction = new Vector2(0, +1); 
     public float speed = 1f;
 
+    // private void OnEnable()
+    //{
+    //    PlayerController.BadGameOver += ShowEndScreen;
+    //    FallManager.GoodGameOver += ShowEndScreen;
+    //}
 
+    //private void OnDisable()
+    //{
+    //    PlayerController.BadGameOver -= ShowEndScreen;
+    //    FallManager.GoodGameOver -= ShowEndScreen;
+    //}
    
     void Update()
     {
