2f48abb0c6f7cfbc387ca239788d6e95192904c3
Shery-Blich <62645308+Shery-Blich@users.noreply.github.com>
2025-12-31 15:35:00 +0200

    Removed score text when finishing game

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -7,8 +7,24 @@ public class ScoreManager : MonoBehaviour
     [SerializeField]
     public TextMeshProUGUI ScoreText;
 
+
+    void OnEnable()
+    {
+        FallManager.GoodGameOver += TurnOffText;
+        PlayerController.BadGameOver += TurnOffText;
+    }
+
+    private void OnDisable()
+    {
+        FallManager.GoodGameOver -= TurnOffText;
+        PlayerController.BadGameOver -= TurnOffText;
+    }
+
     void Update()
     {
-        ScoreText.text = $"Distance To Earth:\n{FallManager.Instance.GetDistanceToEarth()} m\nMemories X {MemoriesManager.Instance.CollectedMemories}";
+        ScoreText.text = $"Distance To Earth:\n{FallManager.Instance.GetDistanceToEarth()} m\nCollected {MemoriesManager.Instance.CollectedMemories} Memories";
     }
+
+    private void TurnOffText() => ScoreText.gameObject.SetActive(false);
+    
 }
